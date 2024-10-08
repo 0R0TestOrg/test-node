2d91eae387aa491daf986804cef84914c9edb69c
Rick Rongen <Rick.Rongen@amexiogroup.com>
2024-10-08 10:56:46 +0200

    feat: added sonar qube support

---
+++ sonar-project.js
@@ -0,0 +1,25 @@
+import scanner from 'sonarqube-scanner';
+
+scanner({
+    options: {
+        'sonar.projectName': 'node-test',
+        'sonar.projectDescription': 'Test app in Node',
+        'sonar.projectKey': '0R0TestOrg_test-node',
+        // 'sonar.javascript.lcov.reportPaths': 'coverage/lcov.info',
+        'sonar.sourceEncoding': 'UTF-8',
+        'sonar.inclusions': [
+            '**/src/**/*',
+        ].join(', '),
+        'sonar.test.inclusions': [
+            '**/test/**/*',
+        ].join(', '),
+        'sonar.test.exclusions': [
+            '**/node_modules/**/*',
+        ].join(', '),
+        'sonar.exclusions': [
+            '**/node_modules/**/*',
+            '**/__mocks__/**/*',
+        ].join(', '),
+    },
+}, () => {
+});
