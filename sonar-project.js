32b619850057913e1350abfb484a941f44581bff
Rick Rongen <Rick.Rongen@amexiogroup.com>
2024-10-08 11:09:07 +0200

    feat: define organisation and exit code

--- sonar-project.js
+++ sonar-project.js
@@ -1,9 +1,11 @@
+import * as process from 'node:process';
 import { scan } from 'sonarqube-scanner';
 
 scan({
     options: {
         'sonar.projectName': 'node-test',
         'sonar.projectDescription': 'Test app in Node',
+        'sonar.organization': '0R0TestOrg',
         'sonar.projectKey': '0R0TestOrg_test-node',
         // 'sonar.javascript.lcov.reportPaths': 'coverage/lcov.info',
         'sonar.sourceEncoding': 'UTF-8',
@@ -22,7 +24,8 @@ scan({
         ].join(', '),
     },
 }).then(() => {
-    console.log("OK")
+    console.log("OK");
 }, (error) => {
-    console.error(error)
+    console.error(error);
+    process.exit(1);
 });
